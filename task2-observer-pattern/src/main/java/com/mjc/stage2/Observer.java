5f82863eeea2c2ffad7479d5d8534bd68122c590
hetot <shaxzod.murtazayev@mail.ru>
2022-12-26 06:00:57 +0300

    try1

--- task2-observer-pattern/src/main/java/com/mjc/stage2/Observer.java
+++ task2-observer-pattern/src/main/java/com/mjc/stage2/Observer.java
@@ -5,4 +5,3 @@ import com.mjc.stage2.event.RectangleEvent;
 public interface Observer {
     void handleEvent(RectangleEvent event);
 }
-
--- task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
+++ task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
@@ -1,5 +1,25 @@
 package com.mjc.stage2.impl;
 
-public class RectangleObserver  {
-    // Write your code here!
+import com.mjc.stage2.Observer;
+import com.mjc.stage2.entity.Rectangle;
+import com.mjc.stage2.entity.RectangleValues;
+import com.mjc.stage2.event.RectangleEvent;
+import com.mjc.stage2.warehouse.RectangleWarehouse;
+
+public class RectangleObserver implements Observer {
+    private static final RectangleWarehouse rectangleWarehouse = RectangleWarehouse.getInstance();
+    @Override
+    public void handleEvent(RectangleEvent event) {
+        Rectangle rectangle = event.getSource();
+        RectangleValues rectangleValues = rectangleWarehouse.get(rectangle.getId());
+        if (rectangleValues != null) {
+            rectangleValues.setPerimeter(
+                    (rectangle.getSideA() + rectangle.getSideB()) * 2
+            );
+            rectangleValues.setSquare(
+                    rectangle.getSideA() * rectangle.getSideB()
+            );
+            rectangleWarehouse.put(rectangle.getId(), rectangleValues);
+        }
+    }
 }
